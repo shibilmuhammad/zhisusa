2c7cf6c677668976fea4adc77db13193da664791
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-07-03 08:52:02 +0530

    gallery page mobile designed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+import PageIntro from "./componets/PageIntro";
+import SimplePageIntro from "./componets/SimplePageIntro";
 import Accomodation from "./pages/Accomodation";
 import Activities from "./pages/Activities";
 import Amenites from "./pages/Amenites";
+import Gallery from "./pages/Gallery";
 import HostEvents from "./pages/HostEvents";
 import ParticipateEvents from "./pages/ParticipateEvents";
 import Works from "./pages/Works";
@@ -8,7 +11,7 @@ import Works from "./pages/Works";
 function App() {
   return (
     <div className="App">
-      <ParticipateEvents />
+      <Gallery />
     </div>
   );
 }
---
+++ frontend/src/pages/Gallery.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import SimplePageIntro from "../componets/SimplePageIntro";
+
+const Gallery = () => {
+	return (
+		<div>
+			<SimplePageIntro
+				data={{
+					thumb: "../images/tent.jpg",
+					title: "Gallery",
+				}}
+			/>
+			<div className="mt-4">
+				<div className="font-poppins flex flex-col items-center gap-2 px-3">
+                    <select className="w-full rounded-lg border-2 border-navyBlue-normal p-2" name="" id="">
+                        <option value="Events">Events</option>
+                        <option value="Activities">Activities</option>
+                    </select>
+					<span className="text-2xl text-navyBlue-normal">Events Gallery</span>
+					<p className="text-navyBlue-dark_hover text-sm text-center">
+						Browse stunning photos of our enchanting weddings, sophisticated art
+						exhibitions, joyful birthday celebrations, and dynamic musical
+						festivals.
+					</p>
+				</div>
+                <div className="grid grid-cols-1 mt-4">
+                    <div>
+                        <img className="h-56 w-full object-cover" src="../images/celebration.jpg" alt="thumb" />
+                    </div>
+                    <div>
+                        <img className="h-56 w-full object-cover" src="../images/wedding.jpg" alt="thumb" />
+                    </div>
+                    <div>
+                        <img className="h-56 w-full object-cover" src="../images/art.jpg" alt="thumb" />
+                    </div>
+                    <div>
+                        <img className="h-56 w-full object-cover" src="../images/music.jpg" alt="thumb" />
+                    </div>
+                </div>
+			</div>
+		</div>
+	);
+};
+
+export default Gallery;
