5ad538e742994c4d57eda4150265c3b632bfb54f
Shibil Muhammad <shibilmuhammad678@gmail.com>
2024-07-09 14:56:28 +0530

    choose your stay responsive done

--- frontend/src/components/MutlitpleRoomBooking.jsx
+++ frontend/src/components/MutlitpleRoomBooking.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const MutlitpleRoomBooking = () => {
   return (
-    <div>
+    <div className='lg:px-10'>
           <div className="mt-4 ">
-        <h1 className="flex justify-center text-xl font-medium font-poppins">
+        <h1 className="flex justify-center text-xl font-medium font-poppins lg:text-2xl">
           CHOOSE YOUR ROOM 2
         </h1>
         <div className="px-2  mt-5">
-          <div className=" flex justify-evenly pt-4 border-y-[.2px] border-y-gray-600 ">
+          <div className=" flex justify-evenly lg:justify-normal lg:space-x-10 pt-4 border-y-[.2px] border-y-gray-600 ">
             <div>
               <p className="font-medium ">
                 Room 1 | <span className="text-xs font-light">2 ADULTS</span>
