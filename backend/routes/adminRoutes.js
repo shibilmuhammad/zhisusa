10c71abbba04c46c468128929df404456f6a02ea
Shibil Muhammad <shibilmuhammad678@gmail.com>
2024-07-16 11:14:56 +0530

    backend done on admin login

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const adminAuthMiddleWare = require('../middleware/authMiddleware')
 const categoryController = require('../controllers/Admin/categoryController');
-
+const loginController = require('../controllers/Admin/loginController')
 
 
 //categories
 router.post('/deletecategory',categoryController.deleteCategory)
-router.get('/getallcategories',categoryController.getCategories)
+router.get('/getallcategories',adminAuthMiddleWare.verifyToken, categoryController.getCategories)
 router.post('/addCategory',categoryController.addCategory)
 
+
+//adminLogin
+router.post('/login',loginController.postLogin)
 module.exports = router
